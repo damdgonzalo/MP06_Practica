ed86da7ba27423f5173b6bfaa4737a1467f1db60
Xavier Castillo <smxxcastillo@gmail.com>
2018-01-26 16:20:24 +0100

    Envas,producte Envas, magatzemEnvas

---
+++ src/mp06_practica/Envas.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp06_practica;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author xaviercastillo
+ */
+public class Envas implements Serializable{
+    
+    private int id_envas;
+    private String id_unitat;
+    private String tipus;
+    private double capacitat;
+    
+    public Envas() {
+    
+    }
+
+    public Envas(int id_envas, String id_unitat, String tipus, double capacitat) {
+        this.id_envas = id_envas;
+        this.id_unitat = id_unitat;
+        this.tipus = tipus;
+        this.capacitat = capacitat;
+    }
+    
+    
+
+    public int getId_envas() {
+        return id_envas;
+    }
+
+    public void setId_envas(int id_envas) {
+        this.id_envas = id_envas;
+    }
+
+    public String getId_unitat() {
+        return id_unitat;
+    }
+
+    public void setId_unitat(String id_unitat) {
+        this.id_unitat = id_unitat;
+    }
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public void setTipus(String tipus) {
+        this.tipus = tipus;
+    }
+
+    public double getCapacitat() {
+        return capacitat;
+    }
+
+    public void setCapacitat(double capacitat) {
+        this.capacitat = capacitat;
+    }
+    
+    
+    
+    
+    
+}
